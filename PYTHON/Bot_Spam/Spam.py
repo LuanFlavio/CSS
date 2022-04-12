2de896d5633f1309de2a5611d56743a3d9231e2b
LuanFlavio <88913472+LuanFlavio@users.noreply.github.com>
2022-04-11 23:05:50 -0300

    Delete PYTHON directory
    Create by commit in origin, but only CSS repository can be visible.

--- PYTHON/Bot_Spam/Spam.py
+++
@@ -1,7 +0,0 @@
-import pyautogui, time
-
-time.sleep(5)
-f = open("Hobby/texto.txt", 'r')
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
--- PYTHON/Curso_Basico/variáveis inteiras ou reais.py
+++
@@ -1,6 +0,0 @@
-num1 = 2
-num2 = 3
-
-print(num1/num2) # conta normal
-print(num1//num2) # conta com o resultado apenas inteiro(ignora depois da vírgula)
-print("%.3f"%(num1/num2)) # o valor antes do "f" indica as casas até onde resultado vai depois da vírgula
